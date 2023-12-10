e298426eacf0bc8bd32fa944fa96d811f6dd71aa
isha133 <ishaacharya16@gmail.com>
2023-12-10 15:30:46 +0530

    Add new folder and files

---
+++ run.bat
@@ -0,0 +1,2 @@
+cd C:\Users\HP\eclipse-workspace\inetbankngV2
+mvn clean install
---
+++ src/main/java/inetbankngV2/inetbankngV2/App.java
@@ -0,0 +1,13 @@
+package inetbankngV2.inetbankngV2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
